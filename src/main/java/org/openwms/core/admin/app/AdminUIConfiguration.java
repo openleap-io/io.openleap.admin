c3c4c66dbbed8439babc6fda3f09ac0640b3e999
Heiko Scherrer <hscherrer@interface21.io>
2021-04-08 15:35:32 +0200

    Added security

--- src/main/java/org/openwms/core/admin/app/AdminUIConfiguration.java
+++ src/main/java/org/openwms/core/admin/app/AdminUIConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005-2020 the original author or authors.
+ * Copyright 2005-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,15 +15,57 @@
  */
 package org.openwms.core.admin.app;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+import java.util.UUID;
 
 /**
  * A AdminUIConfiguration.
  *
  * @author Heiko Scherrer
  */
-@Configuration
+@Configuration(proxyBeanMethods = false)
 @EnableDiscoveryClient
-class AdminUIConfiguration {
+class AdminUIConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final AdminServerProperties adminServer;
+    private final SecurityProperties security;
+
+    AdminUIConfiguration(AdminServerProperties adminServer, SecurityProperties security) {
+        this.adminServer = adminServer;
+        this.security = security;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl(this.adminServer.getContextPath() + "/");
+
+        http.authorizeRequests(
+                (authorizeRequests) -> authorizeRequests.antMatchers(this.adminServer.path("/assets/**")).permitAll()
+                        .antMatchers(this.adminServer.path("/actuator/info")).permitAll()
+                        .antMatchers(this.adminServer.path("/actuator/health")).permitAll()
+                        .antMatchers(this.adminServer.path("/login")).permitAll().anyRequest().authenticated()
+        ).formLogin(
+                (formLogin) -> formLogin.loginPage(this.adminServer.path("/login")).successHandler(successHandler).and() // <3>
+        ).logout((logout) -> logout.logoutUrl(this.adminServer.path("/logout"))).httpBasic(Customizer.withDefaults()) // <4>
+                .csrf().disable()
+                .rememberMe((rememberMe) -> rememberMe.key(UUID.randomUUID().toString()).tokenValiditySeconds(1209600));
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser(security.getUser().getName())
+                .password("{noop}" + security.getUser().getPassword()).roles("USER");
+    }
 }
