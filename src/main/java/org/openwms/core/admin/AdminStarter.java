bda0b71a682ffd209ed5d9c997636e0637c09f96
Heiko Scherrer <scherrer@openwms.org>
2024-09-12 19:26:31 +0200

    #2 Migrated to SpringBoot 3.2.5 and Java 21

--- src/main/java/org/openwms/core/admin/AdminStarter.java
+++ src/main/java/org/openwms/core/admin/AdminStarter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005-2020 the original author or authors.
+ * Copyright 2005-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,17 +17,17 @@ package org.openwms.core.admin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * A AdminStarter.
  *
  * @author Heiko Scherrer
  */
-@Configuration
+@SpringBootApplication
 @EnableAdminServer
-@EnableAutoConfiguration
+@EnableDiscoveryClient
 public class AdminStarter {
 
 	public static void main(String[] args) {
